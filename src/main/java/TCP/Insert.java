66a1db7f887d66f624bc96a09f7a9d247ef9fa3e
Kamil <k.planeta20@gmail.com>
2023-01-12 19:45:22 +0100

    Insert class has been created.

--- src/main/java/TCP/Insert.java
+++ src/main/java/TCP/Insert.java
@@ -1,23 +1,60 @@
 package TCP;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class Insert {
 
+    private Select select;
     public Connection database;
-
     public Insert(Connection database) {
         this.database = database;
+        select = new Select(database);
     }
 
+    public void village(int player_id) {
+        try {
+            String query = "INSERT INTO village (player_id) VALUES(?)";
 
-    static public void insertResult(Connection con, String name, int result) throws SQLException {
-        String query = "INSERT INTO wyniki (imie, punkty) VALUES(?,?)";
+            PreparedStatement stm = database.prepareStatement(query);
+            stm.setInt(1, player_id);
+            stm.executeUpdate();
+        } catch (
+                SQLException e) {
+            System.out.println("Warning: new village insert failed!");
+        }
+    }
 
-        PreparedStatement stm = con.prepareStatement(query);
-        stm.setString(1,name);
-        stm.setInt(2,result);
-        stm.executeUpdate();
-    };
+     public void player(String password, String nickname) {
+         try {
+             String query = "INSERT INTO player (password, nickname) VALUES(?,?)";
+
+             PreparedStatement stm = database.prepareStatement(query);
+             stm.setString(1, password);
+             stm.setString(2, nickname);
+             stm.executeUpdate();
+             int playerID = select.playerID(nickname);
+             village(playerID);
+         } catch (
+                 SQLException e) {
+             System.out.println("Warning: new player insert failed!");
+         }
+     }
+
+
+    public void building(int village_id) {
+        try {
+            String query = "INSERT INTO building (village_id, 1, 1, 1, 1, 1, 1) VALUES(?, ? ,?, ?, ?, ?)";
+
+            PreparedStatement stm = database.prepareStatement(query);
+            stm.setString(1, password);
+            stm.setString(2, nickname);
+            stm.executeUpdate();
+        } catch (
+                SQLException e) {
+            System.out.println("Warning: new player insert failed!");
+        }
+    }
 
 }
