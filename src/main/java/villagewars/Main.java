ad54569185a562873ce2a8ad0cad317a3d1fce04
mateushzet <mateushzett@gmail.com>
2023-01-13 18:11:57 +0100

    cleaned Main

--- src/main/java/villagewars/Main.java
+++ src/main/java/villagewars/Main.java
@@ -5,17 +5,22 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import villagewars.game.controller.ViewController;
+import villagewars.game.player.Player;
 
+import java.util.Scanner;
 public class Main extends Application{
 
 	public static Stage stage;
 	public static Scene scene;
 	public static Timeline animation;
+	public static Player a = new Player("MyNickname", "MyPassword");
 
 	public static void main(String[] args) {
 		launch(args);
 	}
 
+	static public void printVillage(Player player){
+	}
 	@Override
 	public void start(Stage primaryStage) {
 		primaryStage.setResizable(false);
@@ -24,4 +29,5 @@ public class Main extends Application{
 		stage = primaryStage;
 		ViewController.setMenuView();
 	}
+
 }
