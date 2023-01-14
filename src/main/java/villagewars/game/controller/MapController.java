b1746e6839de25ad8a49dd33c81de4087183bb98
KZwolski <kkzwolski@gmail.com>
2023-01-14 20:37:11 +0100

    Added enemy display on map-view and map controller

--- src/main/java/villagewars/game/controller/MapController.java
+++ src/main/java/villagewars/game/controller/MapController.java
@@ -4,12 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-import javax.swing.text.View;
-
 public class MapController {
-
-    @FXML
-    private Label DefencePowerNumber;
 
     @FXML
     private Label attackPowerNumber;
@@ -18,10 +13,39 @@ public class MapController {
     private Label axemanNumber;
 
     @FXML
+    private Label defencePowerNumber;
+
+    @FXML
+    private Label enemyAttackPowerNumber;
+
+    @FXML
+    private Label enemyAxemanNumber;
+
+    @FXML
+    private Label enemyDefencePowerNumber;
+
+    @FXML
+    private Label enemyNickname;
+
+    @FXML
+    private Label enemyPikemanNumber;
+
+    @FXML
+    private Label enemySwordsmanNumber;
+
+    @FXML
     private Label pikemanNumber;
 
     @FXML
+    private Label playersNickname;
+
+    @FXML
     private Label swordsmanNumber;
+
+    @FXML
+    void attackPlayer(ActionEvent event) {
+
+    }
 
     @FXML
     void goToVillageMap(ActionEvent event) {
