92d82c4c8582885dd3ebc1aa000925b6e036f58c
Kamil <k.planeta20@gmail.com>
2023-01-14 18:48:06 +0100

    Client side - Update methods finished

--- src/main/java/villagewars/Main.java
+++ src/main/java/villagewars/Main.java
@@ -16,7 +16,7 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 import java.sql.Connection;
 import java.util.Scanner;
-public class Main extends Application{
+public class Main extends Application {
 
 	static PrintWriter pw;
 	static String host = "localhost";
@@ -31,7 +31,7 @@ public class Main extends Application{
 	//public static Player a = new Player("MyNickname", "MyPassword", con);
 
 	public static void main(String[] args) throws Exception {
-		socket = new Socket(InetAddress.getByName(host),port);
+		socket = new Socket(InetAddress.getByName(host), port);
 
 		//wysylanie do servera
 		pw = new PrintWriter(socket.getOutputStream());
@@ -46,8 +46,9 @@ public class Main extends Application{
 		launch(args);
 	}
 
-	static public void printVillage(Player player){
+	static public void printVillage(Player player) {
 	}
+
 	@Override
 	public void start(Stage primaryStage) {
 		primaryStage.setResizable(false);
@@ -62,236 +63,293 @@ public class Main extends Application{
 		String output;
 		pw.println("login");
 		pw.flush();
-		while ((output = bf.readLine()) == null){}
+		while ((output = bf.readLine()) == null) {
+		}
 		pw.println(nickname);
 		pw.flush();
 
-		while ((output = bf.readLine()) == null){}
+		while ((output = bf.readLine()) == null) {
+		}
 		pw.println(password);
 		pw.flush();
 
-		while ((output = bf.readLine()) == null){}
+		while ((output = bf.readLine()) == null) {
+		}
 		return (output.equals("true")) ? true : false;
 	}
 
 	//---------------- Select methods
 
-	static public String getWoodProduction(){
+	static public String getWoodProduction() {
 		try {
 
 			String output;
 			pw.println("getWoodProduction");
 			pw.flush();
-			while ((output = bf.readLine()) == null){}
+			while ((output = bf.readLine()) == null) {
+			}
 			return output;
 
-		}catch (IOException e) {
+		} catch (IOException e) {
 			return "NaN";
 		}
 	}
 
-	static public String getFoodProduction(){
+	static public String getFoodProduction() {
 		try {
 
 			String output;
 			pw.println("getFoodProduction");
 			pw.flush();
-			while ((output = bf.readLine()) == null){}
+			while ((output = bf.readLine()) == null) {
+			}
 			return output;
 
-		}catch (IOException e) {
+		} catch (IOException e) {
 			return "NaN";
 		}
 	}
 
-	static public String getStoneProduction(){
+	static public String getStoneProduction() {
 		try {
 
 			String output;
 			pw.println("getStoneProduction");
 			pw.flush();
-			while ((output = bf.readLine()) == null){}
+			while ((output = bf.readLine()) == null) {
+			}
 			return output;
 
-		}catch (IOException e) {
+		} catch (IOException e) {
 			return "NaN";
 		}
 	}
 
-	static public String getWood(){
+	static public String getWood() {
 		try {
 
 			String output;
 			pw.println("getWood");
 			pw.flush();
-			while ((output = bf.readLine()) == null){}
+			while ((output = bf.readLine()) == null) {
+			}
 			return output;
 
-		}catch (IOException e) {
+		} catch (IOException e) {
 			return "NaN";
 		}
 	}
 
-	static public String getFood(){
+	static public String getFood() {
 		try {
 
 			String output;
 			pw.println("getFood");
 			pw.flush();
-			while ((output = bf.readLine()) == null){}
+			while ((output = bf.readLine()) == null) {
+			}
 			return output;
 
-		}catch (IOException e) {
+		} catch (IOException e) {
 			return "NaN";
 		}
 	}
 
-	static public String getStone(){
+	static public String getStone() {
 		try {
 
 			String output;
 			pw.println("getStone");
 			pw.flush();
-			while ((output = bf.readLine()) == null){}
+			while ((output = bf.readLine()) == null) {
+			}
 			return output;
 
-		}catch (IOException e) {
+		} catch (IOException e) {
 			return "NaN";
 		}
 	}
 
-	static public String quantityPikeman(){
+	static public String quantityPikeman() {
 		try {
 
 			String output;
 			pw.println("quantityPikeman");
 			pw.flush();
-			while ((output = bf.readLine()) == null){}
+			while ((output = bf.readLine()) == null) {
+			}
 			return output;
 
-		}catch (IOException e) {
+		} catch (IOException e) {
 			return "NaN";
 		}
 	}
 
-	static public String quantitySwordsman(){
+	static public String quantitySwordsman() {
 		try {
 
 			String output;
 			pw.println("quantitySwordsman");
 			pw.flush();
-			while ((output = bf.readLine()) == null){}
+			while ((output = bf.readLine()) == null) {
+			}
 			return output;
 
-		}catch (IOException e) {
+		} catch (IOException e) {
 			return "NaN";
 		}
 	}
 
-	static public String quantityAxeman(){
+	static public String quantityAxeman() {
 		try {
 
 			String output;
 			pw.println("quantityAxeman");
 			pw.flush();
-			while ((output = bf.readLine()) == null){}
+			while ((output = bf.readLine()) == null) {
+			}
 			return output;
 
-		}catch (IOException e) {
+		} catch (IOException e) {
 			return "NaN";
 		}
 	}
 
-	static public String barracksLevel(){
+	static public String barracksLevel() {
 		try {
 
 			String output;
 			pw.println("barracksLevel");
 			pw.flush();
-			while ((output = bf.readLine()) == null){}
+			while ((output = bf.readLine()) == null) {
+			}
 			return output;
 
-		}catch (IOException e) {
+		} catch (IOException e) {
 			return "NaN";
 		}
 	}
 
-	static public String farmLevel(){
+	static public String farmLevel() {
 		try {
 
 			String output;
 			pw.println("farmLevel");
 			pw.flush();
-			while ((output = bf.readLine()) == null){}
+			while ((output = bf.readLine()) == null) {
+			}
 			return output;
 
-		}catch (IOException e) {
+		} catch (IOException e) {
 			return "NaN";
 		}
 	}
 
-	static public String mineLevel(){
+	static public String mineLevel() {
 		try {
 
 			String output;
 			pw.println("mineLevel");
 			pw.flush();
-			while ((output = bf.readLine()) == null){}
+			while ((output = bf.readLine()) == null) {
+			}
 			return output;
 
-		}catch (IOException e) {
+		} catch (IOException e) {
 			return "NaN";
 		}
 	}
 
-	static public String rathausLevel(){
+	static public String rathausLevel() {
 		try {
 
 			String output;
 			pw.println("rathausLevel");
 			pw.flush();
-			while ((output = bf.readLine()) == null){}
+			while ((output = bf.readLine()) == null) {
+			}
 			return output;
 
-		}catch (IOException e) {
+		} catch (IOException e) {
 			return "NaN";
 		}
 	}
 
-	static public String timberCampLevel(){
+	static public String timberCampLevel() {
 		try {
 
 			String output;
 			pw.println("timberCampLevel");
 			pw.flush();
-			while ((output = bf.readLine()) == null){}
+			while ((output = bf.readLine()) == null) {
+			}
 			return output;
 
-		}catch (IOException e) {
+		} catch (IOException e) {
 			return "NaN";
 		}
 	}
 
-	static public String wareHouseLevel(){
+	static public String wareHouseLevel() {
 		try {
 
 			String output;
 			pw.println("wareHouseLevel");
 			pw.flush();
-			while ((output = bf.readLine()) == null){}
+			while ((output = bf.readLine()) == null) {
+			}
 			return output;
 
-		}catch (IOException e) {
+		} catch (IOException e) {
 			return "NaN";
 		}
 	}
 
 	//------------- Update methods
 
-	static public void recruitPikeman(){
-			boolean output;
-			String serverOutput = new String();
-			pw.println("recruitPikeman");
-			pw.flush();
+	static public void recruitPikeman() {
+		pw.println("recruitPikeman");
+		pw.flush();
 	}
+
+	static public void recruitSwordsman() {
+		pw.println("recruitSwordsman");
+		pw.flush();
+	}
+
+	static public void recruitAxeman() {
+		pw.println("recruitAxeman");
+		pw.flush();
+	}
+
+	static public void incrementBarracks() {
+		pw.println("incrementBarracks");
+		pw.flush();
+	}
+
+	static public void incrementFarm() {
+		pw.println("incrementFarm");
+		pw.flush();
+	}
+
+	static public void incrementMine() {
+		pw.println("incrementMine");
+		pw.flush();
+	}
+
+	static public void incrementRathaus() {
+		pw.println("incrementRathaus");
+		pw.flush();
+	}
+
+	static public void incrementTimberCamp() {
+		pw.println("incrementTimberCamp");
+		pw.flush();
+	}
+
+	static public void incrementWareHouse() {
+		pw.println("incrementWareHouse");
+		pw.flush();
+	}
+
 }
